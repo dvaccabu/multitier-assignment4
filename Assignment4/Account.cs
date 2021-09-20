7bfa8730906024d7037dcc2d534d1aac92f280b4
David Vacca <dvaccabu@gmail.com>
2021-09-20 11:59:38 -0400

    Init

---
+++ Assignment4/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Assignment4
+{
+    public class Account
+    {
+        public int AccountNumber { get; }
+        public String FamilyName { get; }
+        public String GivenName { get; }
+        public double Balance { get; }
+
+        public Account(int an, String fn, String gn, double b)
+        {
+            AccountNumber = an;
+            FamilyName = fn;
+            GivenName = gn;
+            Balance = b;
+        }
+    }
+}
---
+++ Assignment4/Validator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Assignment4
+{
+    public class Validator
+    {
+        public Validator()
+        {
+        }
+    }
+}
